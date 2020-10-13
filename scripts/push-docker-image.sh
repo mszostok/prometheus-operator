370ac350d6770055dec4efa31c67b25798632238
Mateusz Szostok <szostok.mateusz@gmail.com>
2020-10-13 23:15:57 +0200

    Change the repo name - testing

--- scripts/push-docker-image.sh
+++ scripts/push-docker-image.sh
@@ -11,13 +11,13 @@ CPU_ARCHS="amd64 arm64 arm"
 export TAG="${GITHUB_REF##*/}"
 
 # Push `-dev` images unless commit is tagged
-export REPO="${REPO:-"quay.io/prometheus-operator/prometheus-operator-dev"}"
-export REPO_PROMETHEUS_CONFIG_RELOADER="${REPO_PROMETHEUS_CONFIG_RELOADER:-"quay.io/prometheus-operator/prometheus-config-reloader-dev"}"
+export REPO="${REPO:-"quay.io/mszostok/prometheus-operator-dev"}"
+export REPO_PROMETHEUS_CONFIG_RELOADER="${REPO_PROMETHEUS_CONFIG_RELOADER:-"quay.io/mszostok/prometheus-config-reloader-dev"}"
 
 # Use main image repositories if TAG is a semver tag or it is a master branch
 if [[ "$TAG" =~ ^v[0-9]+\.[0-9]+ ]] || [ "${TAG}" == "master" ]; then
-	export REPO="quay.io/prometheus-operator/prometheus-operator"
-	export REPO_PROMETHEUS_CONFIG_RELOADER="quay.io/prometheus-operator/prometheus-config-reloader"
+	export REPO="quay.io/mszostok/prometheus-operator"
+	export REPO_PROMETHEUS_CONFIG_RELOADER="quay.io/mszostok/prometheus-config-reloader"
 fi
 
 for arch in ${CPU_ARCHS}; do
